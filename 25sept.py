27c305158d81904baa97f5abaddf0fdc4a96ddcd
dishant1123 <dishantshah1417@gmail.com>
2025-09-30 13:02:58 +0530

    ....

--- 25sept.py
+++ 25sept.py
@@ -39,7 +39,7 @@ c.show()
 """
 # private  : 
 
-class vehicle : 
+"""class vehicle : 
     def __init__(self,type,speed):
         self.__type =type 
         self.__speed =speed  # type  , speed ==> private   + parameterized constructor
@@ -62,3 +62,25 @@ c.display()
 # print(c.__type) # beacuse car type is private , so we can't access it
 c.__type="5-wheelar"
 c.display()
+"""
+
+
+class employees :
+    def __init__(self):
+        self.name ="deyaan"
+        self.salary =90000
+    def show(self):
+        print("name is ",self.name)
+        print("salary is ",self.salary)
+
+class manager(employees):
+    def __init__(self,manager_name):
+        super().__init__()
+        self.manager_name =manager_name
+    
+    def display(self):
+        self.show()
+        print("manager name is ",self.manager_name)
+
+m=manager("kabir")
+m.display()
