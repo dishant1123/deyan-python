592e39dd3100167a344f0be0b85fe9db053ae0c7
dishant1123 <dishantshah1417@gmail.com>
2026-02-02 15:44:51 +0530

    ....

--- 2feb.py
+++ 2feb.py
@@ -38,3 +38,19 @@ print(b)
 c=np.arange(1,33).reshape(2,2,2,4)
 print(c)
 """
+# slicing : 
+a= np.array([1,23,45,67,89,22,56,99])
+
+# index number start  with  0    ==> pos  ==> l  to  r 
+# neg index number   start  -1:  ==> r  to l 
+
+print(a[3])
+print(a[-1])
+print(a[3:5])  # 3 start  index  5 end index
+print(a[2 :6 :2 ])  # 2 start  index  6 end index  2 step
+
+print(a[ : :2]) 
+print(a[ : :3])
+print(a[ : : -2])
+print(a[ : : -1])
+ 
